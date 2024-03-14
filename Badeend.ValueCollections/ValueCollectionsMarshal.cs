94ef61472e44151929a39d9fae5eadaefa7448e9
Dave Bakker <github@davebakker.io>
2024-03-14 11:09:08 +0100

    Render as pretty alert boxes in documentation

--- Badeend.ValueCollections/ValueCollectionsMarshal.cs
+++ Badeend.ValueCollections/ValueCollectionsMarshal.cs
@@ -11,7 +11,8 @@ public static class ValueCollectionsMarshal
 	/// Create a new <see cref="ValueSlice{T}"/> using the provided mutable array
 	/// as its backing store. This operation does not allocate any memory.
 	///
-	/// <b>Warning!</b> Ownership of the array is moved into the ValueSlice. It is the caller's
+	/// > [!WARNING]
+	/// > Ownership of the array is moved into the ValueSlice. It is the caller's
 	/// responsibility to never mutate the array ever again.
 	/// </summary>
 	public static ValueSlice<T> AsValueSlice<T>(T[] items) => new(items);
@@ -20,7 +21,8 @@ public static class ValueCollectionsMarshal
 	/// Create a new <see cref="ValueSlice{T}"/> using the provided mutable list
 	/// as its backing store. This operation does not allocate any memory.
 	///
-	/// <b>Warning!</b> Ownership of the list is moved into the ValueSlice. It is the caller's
+	/// > [!WARNING]
+	/// > Ownership of the list is moved into the ValueSlice. It is the caller's
 	/// responsibility to never mutate the list ever again.
 	/// </summary>
 	public static ValueSlice<T> AsValueSlice<T>(List<T> items) => new(UnsafeHelpers.GetBackingArray(items), 0, items.Count);
@@ -31,7 +33,8 @@ public static class ValueCollectionsMarshal
 	/// memory for the new ValueList instance. For the actual content it reuses
 	/// the provided array instead of copying it over.
 	///
-	/// <b>Warning!</b> Ownership of the array is moved into the ValueList. It is the caller's
+	/// > [!WARNING]
+	/// > Ownership of the array is moved into the ValueList. It is the caller's
 	/// responsibility to never mutate the array ever again.
 	/// </summary>
 	public static ValueList<T> AsValueList<T>(T[] items) => ValueList<T>.FromArrayUnsafe(items);
@@ -43,7 +46,8 @@ public static class ValueCollectionsMarshal
 	/// the backing array of the provided list rather than of copying the items
 	/// over.
 	///
-	/// <b>Warning!</b> Ownership of the list is moved into the ValueList. It is the caller's
+	/// > [!WARNING]
+	/// > Ownership of the list is moved into the ValueList. It is the caller's
 	/// responsibility to never mutate the list ever again.
 	/// </summary>
 	public static ValueList<T> AsValueList<T>(List<T> items) => ValueList<T>.FromArrayUnsafe(UnsafeHelpers.GetBackingArray(items), items.Count);
@@ -55,7 +59,8 @@ public static class ValueCollectionsMarshal
 	/// instance. For the actual content it reuses the provided array instead of
 	/// copying it over.
 	///
-	/// <b>Warning!</b> Ownership of the array is moved into the ValueListBuilder.
+	/// > [!WARNING]
+	/// > Ownership of the array is moved into the ValueListBuilder.
 	/// It is the caller's responsibility to never touch the array ever again.
 	/// </summary>
 	public static ValueListBuilder<T> AsValueListBuilder<T>(T[] items) => ValueListBuilder<T>.FromArrayUnsafe(items);
@@ -67,14 +72,17 @@ public static class ValueCollectionsMarshal
 	/// instance. For the actual content it reuses the backing array of the
 	/// provided list instead of copying it over.
 	///
-	/// <b>Warning!</b> Ownership of the list is moved into the ValueListBuilder.
+	/// > [!WARNING]
+	/// > Ownership of the list is moved into the ValueListBuilder.
 	/// It is the caller's responsibility to never touch the list ever again.
 	/// </summary>
 	public static ValueListBuilder<T> AsValueListBuilder<T>(List<T> items) => ValueListBuilder<T>.FromListUnsafe(items);
 
 	/// <summary>
-	/// Update the count of the <paramref name="builder"/>. When increasing the
-	/// count, this exposes uninitialized data.
+	/// Update the count of the <paramref name="builder"/>.
+	///
+	/// > [!WARNING]
+	/// > When increasing the count, this may expose uninitialized, garbage data.
 	/// </summary>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void SetCount<T>(ValueListBuilder<T> builder, int count) => builder.SetCountUnsafe(count);
@@ -83,7 +91,8 @@ public static class ValueCollectionsMarshal
 	/// Get a <see cref="Span{T}"/> view over the current data in the
 	/// <paramref name="builder"/>.
 	///
-	/// <b>Warning!</b> The builder should not be accessed while the span is in
+	/// > [!WARNING]
+	/// > The builder should not be accessed while the span is in
 	/// use. Unlike the <see cref="List{T}"/> equivalent of this method
 	/// (<c>CollectionsMarshal.AsSpan</c>), even just <em>reading</em> from the
 	/// builder might trigger undefined behavior.
